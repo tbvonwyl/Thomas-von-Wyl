18664ca1ac043296a54a316c82157b0708f8a288
oscar <oscar@oscar.(none)>
2013-05-20 07:07:44 +0200

    Testat 2 EBV FS13

--- process_frame.c
+++ process_frame.c
@@ -1,11 +1,11 @@
 /* Copying and distribution of this file, with or without modification,
- * are permitted in any medium without royalty. This file is offered as-is,
- * without any warranty.
- */
+* are permitted in any medium without royalty. This file is offered as-is,
+* without any warranty.
+*/
 
 /*! @file process_frame.c
- * @brief Contains the actual algorithm and calculations.
- */
+* @brief Contains the actual algorithm and calculations.
+*/
 
 /* Definitions specific to this application. Also includes the Oscar main header file. */
 #include "template.h"
@@ -27,6 +27,72 @@ void ProcessFrame(uint8 *pInputImg)
 	struct OSC_PICTURE Pic1, Pic2;//we require these structures to use Oscar functions
 	struct OSC_VIS_REGIONS ImgRegions;//these contain the foreground objects
 
+
+	// Schwellwertbestimmung nach Otsu
+
+	uint8 * p = data.u8TempImage[GRAYSCALE];
+
+	uint32 Hist[256];
+	int32 iHist = 0;
+
+	int32 iOtsu = 0;
+	uint8 iOtsuMax = 0;
+	int32 jOtsu = 0;
+
+	uint32 W0 = 0;
+	uint32 W1 = 0;
+	uint32 M0 = 0;
+	uint32 M1 = 0;
+
+	float tempSigma = 0;
+	float sigma = 0;
+	float sigmaMax = 0;
+
+
+	memset(Hist, 0, sizeof(Hist));
+
+
+	// Berechnung des Histogramms
+	for(iHist = 0; iHist < siz; iHist++)
+	{
+		Hist[p[iHist]] += 1;
+	}
+
+	// Otsu-Algorithmus
+	for(iOtsu = 0; iOtsu < 255; iOtsu++)
+	{
+		W0 = 0;
+		W1 = 0;
+
+		// Bestimmung Parameter 0
+		for(jOtsu = 0; jOtsu <= iOtsu; jOtsu++)
+		{
+			W0 += Hist[jOtsu];
+			M0 += Hist[jOtsu] * jOtsu;
+		}
+
+		// Bestimmung Parameter 1
+		for(jOtsu = iOtsu + 1; jOtsu < 255; jOtsu++)
+		{
+			W1 += Hist[jOtsu];
+			M1 += Hist[jOtsu] * jOtsu;
+		}
+
+		// Berechnung Sigmawert in Abhängigkeit der Parameter
+		//tempSigma = ((M0 / W0) - (M1 / W1)) * ((M0 / W0) - (M1 / W1));
+		//sigma = W0 * W1 * tempSigma;
+
+		sigma = W0 * W1 * (((M0 / W0) - (M1 / W1)) * ((M0 / W0) - (M1 / W1)));
+
+		// Maximaler Sigmawert speichern, sowie zugehöriger Index
+		if (sigma > sigmaMax)
+		{
+			sigmaMax = sigma;
+			iOtsuMax = iOtsu;
+		}
+	}
+
+
 	if(data.ipc.state.nStepCounter == 1)
 	{
 		/* this is the first time we call this function */
@@ -37,53 +103,26 @@ void ProcessFrame(uint8 *pInputImg)
 	}
 	else
 	{
-		/* this is the default case */
+		// Manuelle Schwelwertbestimmung
+
 		for(r = 0; r < siz; r+= nc)/* we strongly rely on the fact that them images have the same size */
 		{
 			for(c = 0; c < nc; c++)
 			{
-				/* first determine the foreground estimate */
-				data.u8TempImage[THRESHOLD][r+c] = abs((short) data.u8TempImage[GRAYSCALE][r+c]-(short) data.u8TempImage[BACKGROUND][r+c]) < data.ipc.state.nThreshold ? 0 : 0xff;
-
-				/* now depending on the foreground estimate ... */
-				if(data.u8TempImage[THRESHOLD][r+c]) {
-					/* ... either in case foreground is detected -> do not update the background but increase the foreground counter */
-					if(data.u8TempImage[FGRCOUNTER][r+c] < MaxForeground) {
-						data.u8TempImage[FGRCOUNTER][r+c]++;
-					} else {
-						/* if counter reaches max -> set current image to background */
-						data.u8TempImage[FGRCOUNTER][r+c] = 0;
-						data.u8TempImage[BACKGROUND][r+c] = data.u8TempImage[GRAYSCALE][r+c];
-					}
-				} else {/* ...or in case background is detected -> decrease foreground counter and update background as usual */					
-					if(0 < data.u8TempImage[FGRCOUNTER][r+c]) {
-						data.u8TempImage[FGRCOUNTER][r+c]--;
-					}
-					/* now update the background image; the value of background should be corrected by the following difference (* 1/128) */
-					short Diff = Beta*((short) data.u8TempImage[GRAYSCALE][r+c] - (short) data.u8TempImage[BACKGROUND][r+c]);
-
-					if(abs(Diff) >= 128) //we will have a correction - apply it (this also avoids the "bug" that -1 >> 1 = -1)
-						data.u8TempImage[BACKGROUND][r+c] = (uint8) ((short) data.u8TempImage[BACKGROUND][r+c] + (Diff >> Shift));//first cast to (short) because Diff can be negative then cast to uint8
-																																  //we do no explicit min(255, max(0, ** )) statement; this should not happen
-					else //due to the division by 128 the correction would be zero -> thus add/subtract at least unity
-					{
-						if(Diff > 0 && data.u8TempImage[BACKGROUND][r+c] < 255)
-								data.u8TempImage[BACKGROUND][r+c] += 1;
-						else if(Diff < 0 && data.u8TempImage[BACKGROUND][r+c] > 1)
-								data.u8TempImage[BACKGROUND][r+c] -= 1;
-					}
-				}
+				// first determine the foreground estimate
+				data.u8TempImage[THRESHOLD][r+c] = data.u8TempImage[GRAYSCALE][r+c] > iOtsuMax ? 0 : 0xff;
 			}
 		}
 
+
 		/*
 		{
-			//for debugging purposes we log the background values to console out
-			//we chose the center pixel of the image (adaption to other pixel is straight forward)
-			int offs = nc*(OSC_CAM_MAX_IMAGE_HEIGHT/2)/2+nc/2;
+		//for debugging purposes we log the background values to console out
+		//we chose the center pixel of the image (adaption to other pixel is straight forward)
+		int offs = nc*(OSC_CAM_MAX_IMAGE_HEIGHT/2)/2+nc/2;
 
-			OscLog(INFO, "%d %d %d %d %d\n", (int) data.u8TempImage[GRAYSCALE][offs], (int) data.u8TempImage[BACKGROUND][offs], (int) data.u8TempImage[BACKGROUND][offs]-data.ipc.state.nThreshold,
-											 (int) data.u8TempImage[BACKGROUND][offs]+data.ipc.state.nThreshold, (int) data.u8TempImage[FGRCOUNTER][offs]);
+		OscLog(INFO, "%d %d %d %d %d\n", (int) data.u8TempImage[GRAYSCALE][offs], (int) data.u8TempImage[BACKGROUND][offs], (int) data.u8TempImage[BACKGROUND][offs]-data.ipc.state.nThreshold,
+		(int) data.u8TempImage[BACKGROUND][offs]+data.ipc.state.nThreshold, (int) data.u8TempImage[FGRCOUNTER][offs]);
 		}
 		*/
 
@@ -93,8 +132,8 @@ void ProcessFrame(uint8 *pInputImg)
 			{
 				unsigned char* p = &data.u8TempImage[THRESHOLD][r+c];
 				data.u8TempImage[EROSION][r+c] = *(p-nc-1) & *(p-nc) & *(p-nc+1) &
-												 *(p-1)    & *p      & *(p+1)    &
-												 *(p+nc-1) & *(p+nc) & *(p+nc+1);
+				*(p-1) & *p & *(p+1) &
+				*(p+nc-1) & *(p+nc) & *(p+nc+1);
 			}
 		}
 
@@ -104,8 +143,8 @@ void ProcessFrame(uint8 *pInputImg)
 			{
 				unsigned char* p = &data.u8TempImage[EROSION][r+c];
 				data.u8TempImage[DILATION][r+c] = *(p-nc-1) | *(p-nc) | *(p-nc+1) |
-												  *(p-1)    | *p      | *(p+1)    |
-												  *(p+nc-1) | *(p+nc) | *(p+nc+1);
+				*(p-1) | *p | *(p+1) |
+				*(p+nc-1) | *(p+nc) | *(p+nc+1);
 			}
 		}
 
@@ -135,30 +174,29 @@ void ProcessFrame(uint8 *pInputImg)
 }
 
 
-/* Drawing Function for Bounding Boxes; own implementation because Oscar only allows colored boxes; here in Gray value "Color"  */
+/* Drawing Function for Bounding Boxes; own implementation because Oscar only allows colored boxes; here in Gray value "Color" */
 /* should only be used for debugging purposes because we should not drawn into a gray scale image */
 OSC_ERR OscVisDrawBoundingBoxBW(struct OSC_PICTURE *picIn, struct OSC_VIS_REGIONS *regions, uint8 Color)
 {
-	 uint16 i, o;
-	 uint8 *pImg = (uint8*)picIn->data;
-	 const uint16 width = picIn->width;
-	 for(o = 0; o < regions->noOfObjects; o++)//loop over regions
-	 {
-		 /* Draw the horizontal lines. */
-		 for (i = regions->objects[o].bboxLeft; i < regions->objects[o].bboxRight; i += 1)
-		 {
-				 pImg[width * regions->objects[o].bboxTop + i] = Color;
-				 pImg[width * (regions->objects[o].bboxBottom - 1) + i] = Color;
-		 }
+	uint16 i, o;
+	uint8 *pImg = (uint8*)picIn->data;
+	const uint16 width = picIn->width;
+	for(o = 0; o < regions->noOfObjects; o++)//loop over regions
+	{
+		/* Draw the horizontal lines. */
+		for (i = regions->objects[o].bboxLeft; i < regions->objects[o].bboxRight; i += 1)
+		{
+			pImg[width * regions->objects[o].bboxTop + i] = Color;
+			pImg[width * (regions->objects[o].bboxBottom - 1) + i] = Color;
+		}
 
-		 /* Draw the vertical lines. */
-		 for (i = regions->objects[o].bboxTop; i < regions->objects[o].bboxBottom-1; i += 1)
-		 {
-				 pImg[width * i + regions->objects[o].bboxLeft] = Color;
-				 pImg[width * i + regions->objects[o].bboxRight] = Color;
-		 }
-	 }
-	 return SUCCESS;
+		/* Draw the vertical lines. */
+		for (i = regions->objects[o].bboxTop; i < regions->objects[o].bboxBottom-1; i += 1)
+		{
+			pImg[width * i + regions->objects[o].bboxLeft] = Color;
+			pImg[width * i + regions->objects[o].bboxRight] = Color;
+		}
+	}
+	return SUCCESS;
 }
-
 
